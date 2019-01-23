66b09e48a5b3f345437067b953295abe11926599
ivorita <630150119@qq.com>
2019-01-23 17:01:46 +0800

    First commit.

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "HmEaGw53mejBLgMXsEEi5jMBkz9pnE4P4u9KjXShoK8N";
+    public static final String SDK_KEY = "83EXcczB39nJr6RsX3zGvg4K9NRMNMs3ed7V4eNRPwyX";
+}
